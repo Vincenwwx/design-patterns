ecb983c5d84953f6455cc4d349e91c41d43cea8e
Vincenwwx <berneywwx@gmail.com>
2024-01-02 00:30:28 +0100

    Add header guard

--- include/pizza.h
+++ include/pizza.h
@@ -1,34 +1,37 @@
+#pragma once
+
 #include <string>
 
 namespace fact {
 class Pizza
 {
 public:
+    virtual ~Pizza() {};
     virtual std::string type() const = 0;
 };
 
 class CheesePizza : public Pizza
 {
 public:
-    std::string type() const override { return "Cheese pizza"; };
+    std::string type() const override { return "cheese"; };
 };
 
 class PepperoniPizza : public Pizza
 {
 public:
-    std::string type() const override { return "Pepperoni pizza"; }
+    std::string type() const override { return "pepperoni"; }
 };
 
 class HawaiiPizza : public Pizza
 {
 public:
-    std::string type() const override { return "Hawaii pizza"; }
+    std::string type() const override { return "hawaii"; }
 };
 
 class VeggiePizza : public Pizza
 {
 public:
-    std::string type() const override { return "Veggie pizza"; }
+    std::string type() const override { return "veggie"; }
 };
 
 } // namespace fact
--- include/simple_pizza_factory.h
+++ include/simple_pizza_factory.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "pizza.h"
 
 #include <memory>
