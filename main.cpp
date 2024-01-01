2229db9f1233aa78d3e395b3187d3cb86b98926d
Vincenwwx <berneywwx@gmail.com>
2024-01-02 00:30:47 +0100

    Create main src file

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include "pizza.h"
+#include "simple_pizza_factory.h"
+
+#include <iostream>
+
+int main() {
+    const auto pizza_factory = std::make_unique<fact::SimplePizzaFactory>();
+    std::array<std::string, 5> PIZZA_TYPES{"cheese", "pepperoni", "veggie", "hawaii", ""};
+
+    std::unique_ptr<fact::Pizza> pizza;
+    bool pass{true};
+    for(const auto &pizza_type : PIZZA_TYPES) {
+        pizza = pizza_factory->createPizza(pizza_type);
+        if(pizza == nullptr) {
+            if(pizza_type != "") {
+                std::cerr << "Failed create NULL pizza" << '\n';
+                pass = false;
+            }
+        }
+        else if(pizza->type() != pizza_type) {
+            pass = false;
+            std::cerr << "Wrong pizza created: " << pizza->type()
+                      << "(want: " << pizza_type << ")\n";
+        }
+    }
+
+    if(pass)
+        std::cout << "All tests passed." << std::endl;
+    return 0;
+}
