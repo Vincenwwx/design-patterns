caa9afa5b35d52ea98c0ca289f873dcb99af14df
Vincenwwx <berneywwx@gmail.com>
2024-01-03 01:32:00 +0100

    Add some member functions to Pizza class

--- include/pizza.h
+++ include/pizza.h
@@ -1,13 +1,19 @@
 #pragma once
 
 #include <string>
+#include <iostream>
 
 namespace fact {
 class Pizza
 {
 public:
     virtual ~Pizza() {};
+
     virtual std::string type() const = 0;
+    void prepare() const { std::cout << "Prepare " << type() << " pizza" << std::endl; }
+    void bake() const { std::cout << "Bake " << type() << " pizza" << std::endl; }
+    void cut() const { std::cout << "Cut " << type() << " pizza" << std::endl; }
+    void pack() const { std::cout << "Pack " << type() << " pizza" << std::endl; }
 };
 
 class CheesePizza : public Pizza
