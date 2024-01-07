6154b5ce09333dcc3d4e9b8046a2326b447d3a84
Vincenwwx <berneywwx@gmail.com>
2024-01-07 17:35:15 +0100

    Add new pizza classs for abstract factory

--- include/pizza.h
+++ include/pizza.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "pizza_ingredient_factory.h"
 #include <string>
 #include <iostream>
 
@@ -92,5 +93,67 @@ public:
     std::string type() const override { return "Chicago stype pepperoni pizza"; }
 };
 } // namespace fact::method
+
+namespace abs
+{
+/**
+ * @brief Base class defined for present the abstract factory method. Unlike the factory method
+ * pattern, the `(NewYork|Chicago|..)Style(Cheese|Pepperoni|...)Pizza` classes are abstracted into
+ * `(Cheese|Pepperoni)Pizza` classes, combining with a input factory to control the region.
+ */
+class Pizza
+{
+public:
+    Pizza(std::unique_ptr<PizzaIngredientFactory> &&factory, bool verbose=false) : 
+        style_(factory->style()),
+        dough_(factory->createDough()),
+        sauce_(factory->createSauce()),
+        cheese_(factory->createCheese()),
+        verbose_(verbose)
+    {
+    }
+    virtual ~Pizza() = default;
+
+    virtual std::string type() const = 0;
+    void prepare() const {
+        if(verbose_)
+            std::cout << "Prepare " << style_ << "-style " << type() << " pizza" << std::endl;
+    }
+    void bake() const { if(verbose_) std::cout << "Bake " << style_ << "-style " << type() << std::endl; }
+    void cut() const { if(verbose_) std::cout << "Cut " << style_ << "-style " << type() << std::endl; }
+    void pack() const { if(verbose_) std::cout << "Pack " << style_ << "-style " << type() << std::endl; }
+
+private:
+    std::string style_;
+    std::unique_ptr<ingredient::Dough> dough_;
+    std::unique_ptr<ingredient::Sauce> sauce_;
+    std::unique_ptr<ingredient::Cheese> cheese_;
+
+    bool verbose_;
+};
+
+class CheesePizza : public Pizza
+{
+public:
+    CheesePizza(std::unique_ptr<PizzaIngredientFactory> &&factory, bool verbose=false)
+        : Pizza(std::move(factory), verbose)
+    {
+    }
+
+    std::string type() const override { return "cheese pizza"; }
+};
+
+class PepperoniPizza : public Pizza
+{
+public:
+    PepperoniPizza(std::unique_ptr<PizzaIngredientFactory> &&factory, bool verbose=false)
+        : Pizza(std::move(factory), verbose)
+    {
+    }
+
+    std::string type() const override { return "pepperoni pizza"; }
+};
+
+} // namespace fact::abs
 
 } // namespace fact
