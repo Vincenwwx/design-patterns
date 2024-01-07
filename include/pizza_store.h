db81b4b481e492e96e0574b66c803759650f2b2b
Vincenwwx <berneywwx@gmail.com>
2024-01-07 17:36:34 +0100

    Create new pizza stores for abstract factory

--- include/pizza_store.h
+++ include/pizza_store.h
@@ -3,15 +3,19 @@
 #include "pizza.h"
 #include <string_view>
 
-namespace fact::method
+namespace fact
+{
+namespace method
 {
 /**
  * @brief In this file, we demonstrate the factory method pattern, by which a (pure) virtual
  * function (or method) is defined in the base class and decided in the derived class.
  */
+
 class PizzaStore
 {
 public:
+    virtual ~PizzaStore() = default;
     /**
      * Order a pizza.
      * 
@@ -48,4 +52,45 @@ protected:
     std::unique_ptr<Pizza> createPizza(std::string_view type, bool verbose=false) const override;
 };
 
-} /// namespace fact::method
+} // namespace fact::method
+
+namespace abs
+{
+
+class PizzaStore
+{
+public:
+    PizzaStore() = default;
+    virtual ~PizzaStore() = default;
+
+    std::unique_ptr<Pizza> orderPizza(std::string_view type) const;
+
+private:
+    // All pizza stores share the same pizza categories (i.e. cheese and pepperoni), while
+    // differentiate in the regional favors by using differnt ingredient factories.
+    std::unique_ptr<Pizza> createPizza(std::string_view type, bool verbose) const;
+
+    virtual std::unique_ptr<PizzaIngredientFactory> getFactory() const = 0;
+};
+
+class NewYorkStylePizzaStore : public PizzaStore
+{
+private:
+    std::unique_ptr<PizzaIngredientFactory> getFactory() const override
+    {
+        return std::make_unique<NewYorkStylePizzaIngredientFactory>();
+    }
+};
+
+class ChicagoStylePizzaStore : public PizzaStore
+{
+private:
+    std::unique_ptr<PizzaIngredientFactory> getFactory() const override
+    {
+        return std::make_unique<ChicagoStylePizzaIngredientFactory>();
+    }
+};
+
+} // namespace fact::abs
+
+} // namespace fact
--- src/pizza_store.cpp
+++ src/pizza_store.cpp
@@ -1,9 +1,11 @@
 #include "pizza_store.h"
 
-namespace fact::method
+namespace fact
 {
 static const bool VERBOSE = false;
 
+namespace method
+{
 std::unique_ptr<Pizza> PizzaStore::orderPizza(std::string_view type) const {
     auto pizza = createPizza(type, VERBOSE);
     pizza->prepare();
@@ -37,3 +39,28 @@ std::unique_ptr<Pizza> ChicagoStylePizzaStore::createPizza(
 }
 
 } // namespace fact::method
+
+namespace abs
+{
+std::unique_ptr<Pizza> PizzaStore::orderPizza(std::string_view type) const {
+    auto pizza = createPizza(type, VERBOSE);
+    pizza->prepare();
+    pizza->bake();
+    pizza->cut();
+    pizza->pack();
+
+    return pizza;
+}
+
+std::unique_ptr<Pizza> PizzaStore::createPizza(std::string_view type, bool verbose) const {
+    std::unique_ptr<Pizza> pizza;
+    if(type == "cheese")
+        pizza.reset(new CheesePizza{getFactory(), verbose});
+    else if(type == "pepperoni")
+        pizza.reset(new PepperoniPizza{getFactory(), verbose});
+    return pizza;
+}
+
+} // namespace fact::abs
+
+} // namespace fact
