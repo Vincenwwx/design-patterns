f28b4ec474cf0b27ece37a33a16d8788d61113b9
Vincenwwx <berneywwx@gmail.com>
2024-01-24 00:40:46 +0100

    Create runnable application

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
+#include "Menu.h"
+#include "Waitress.h"
 #include "DinerMenu.h"
-#include "PancakHouseMenu.h"
+#include "PancakeHouseMenu.h"
+
+#include <iostream>
+#include <memory>
+
 
 int main()
 {
+    std::vector<std::unique_ptr<iter::Menu>> menu_list{};
+    menu_list.push_back(std::make_unique<iter::PancakeHouseMenu>());
+    menu_list.push_back(std::make_unique<iter::DinerMenu>());
+
+    auto const waitress = std::make_unique<iter::Waitress>(std::move(menu_list));
+    waitress->printMenu();
+
     return 0;
 }
