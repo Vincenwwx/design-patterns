6a7fd6e4ac1504d27d758e7c9f40fdd5a2412b6c
Vincenwwx <berneywwx@gmail.com>
2024-01-26 02:00:04 +0100

    Add monitor

--- main.cpp
+++ main.cpp
@@ -4,23 +4,23 @@
 #include "TelevisionCommand.h"
 #include "ControlCenter.h"
 #include <memory>
+#include <ranges>
 
 int main()
 {
-    furniture::Light light{};
-    furniture::Television television{};
+    auto light = std::make_shared<furniture::Light>();
+    auto television = std::make_shared<furniture::Television>();
+    std::vector<std::shared_ptr<furniture::Furniture>> monitor{light, television};
 
     cust::ControlCenter control{};
-    control.setCommand(0, std::make_unique<comd::TurnOnLight>(light));
-    control.setCommand(1, std::make_unique<comd::TurnOnTelevision>(television));
+    control.setCommand(0, std::make_unique<comd::TurnOnLight>(*light));
+    control.setCommand(1, std::make_unique<comd::TurnOnTelevision>(*television));
 
     control.executeCommand(0);
-    light.printStatus();
-    television.printStatus();
+    std::ranges::for_each(monitor, &furniture::Furniture::printStatus);
 
     control.executeCommand(1);
-    light.printStatus();
-    television.printStatus();
+    std::ranges::for_each(monitor, &furniture::Furniture::printStatus);
 
     return 0;
 }
